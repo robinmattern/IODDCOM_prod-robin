fc7955c52d7617c8748bb80f6d449499dd673489
robinmattern <robin.mattern@gmail.com>
2022-12-13 10:18:04 -0500

    .(21213.01 | Move app 1s3 to 1s2

--- server2/1s2_iodd-mysql-getjson/getMembers.sh
+++ server2/1s2_iodd-mysql-getjson/getMembers.sh
@@ -4,5 +4,7 @@
 # mysqlsh -u root -pFormR\!1234 --json=pretty --database=io --sql -f getMembers.sql
 # mysqlsh -u root -pFormR\!1234 --json=raw    --database=io --js  -f getMembers.sjs
   mysqlsh -u root -pFormR\!1234 --json=pretty --database=io --sql  --execute "SELECT * FROM members"
+  mysqlsh -u root -pFormR\!1234 --json=pretty --database=io --sql  --execute "SELECT * FROM projects"
+  mysqlsh -u root -pFormR\!1234 --json=pretty --database=io --sql  --execute "SELECT * FROM members_projects"
 
 
