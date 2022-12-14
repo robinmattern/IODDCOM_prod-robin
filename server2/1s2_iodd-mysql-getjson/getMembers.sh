f851b75ac03010e31678f029ce74772a1aeac804
robinmattern <robin.mattern@gmail.com>
2022-12-13 19:11:49 -0500

    .(21213.02 | Create db.json using mysqlsh

--- server2/1s2_iodd-mysql-getjson/getMembers.sh
+++ server2/1s2_iodd-mysql-getjson/getMembers.sh
@@ -3,8 +3,8 @@
 # mysqlsh -u root -pFormR\!1234 --json=raw                  --sql -f getMembers.sql
 # mysqlsh -u root -pFormR\!1234 --json=pretty --database=io --sql -f getMembers.sql
 # mysqlsh -u root -pFormR\!1234 --json=raw    --database=io --js  -f getMembers.sjs
-  mysqlsh -u root -pFormR\!1234 --json=pretty --database=io --sql  --execute "SELECT * FROM members"
-  mysqlsh -u root -pFormR\!1234 --json=pretty --database=io --sql  --execute "SELECT * FROM projects"
-  mysqlsh -u root -pFormR\!1234 --json=pretty --database=io --sql  --execute "SELECT * FROM members_projects"
+  mysqlsh -u root -pFormR\!1234 --json=pretty --database=io --sql  --execute "SELECT * FROM members"                # .(21213.02.1 | Create db.json using mysqlsh)
+  mysqlsh -u root -pFormR\!1234 --json=pretty --database=io --sql  --execute "SELECT * FROM projects"               # .(21213.02.2)
+  mysqlsh -u root -pFormR\!1234 --json=pretty --database=io --sql  --execute "SELECT * FROM members_projects_view"  # .(21213.02.3)
 
 
