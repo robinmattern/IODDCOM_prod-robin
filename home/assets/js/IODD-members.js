6ffa2a62966c77339f74b281e5ea3cf5913f3ea5
robinmattern <robin.mattern@gmail.com>
2022-12-18 18:46:02 -0500

    .(21218.02 | Add in Rick's 12/17/22 changes (I think)

--- home/assets/js/IODD-members.js
+++ home/assets/js/IODD-members.js
@@ -1,46 +1,13 @@
-/*\
-##=========+====================+================================================+
-##RD         IODD-members       | Format IODD Members table
-##RFILE    +====================+=======+===============+======+=================+
-##FD   IODD.Members.js          |   4757| 12/08/22  9:30|   101| p1.00-21208-0930
-##FD   IODD.Members.js          |   5772| 12/09/22 13:15|   105| p1.01-21209-1315
-##FD   IODD-members.js          |   6007| 12/10/22 16:40|   119| p1.02-21210.1640
-##DESC     .--------------------+-------+---------------+------+-----------------+
-#            Read Members Data and format it
-#
-##LIC      .--------------------+----------------------------------------------+
-#            Copyright (c) 2022 8020Data-formR * Released under
-#            MIT License: http://www.opensource.org/licenses/mit-license.php
-
-##FNCS     .--------------------+----------------------------------------------+
-#            fmtMembers         | Format array of Member records
-#              fmtMember        | Format each Member record
-#            parseJSON          | Convert .json text into pJSON object
-#              readFile         | Read a file (no error handling)                       // .(21210.03.1)
-#            sortitems          | Sort Members by LasName, FirstName                    // .(21209.04.1)
-#
-##CHGS     .--------------------+----------------------------------------------+
-# .(21208.01 12/08/21 RAM  9:30a| Created
-# .(21209.04 12/09/21 RJS  1:15p| Add sortitems
-# .(21210.03 12/10/21 RAM  4:40p| Add readFile
-# .(21213.06 12/13/21 RAM 11:20p| Read db.json.js, not IODD-members_u2a.json.js
-
-##PRGM     +====================+===============================================+
-##ID 69.600. Main               |
-##SRCE     +====================+===============================================+
-\*/
-//========================================================================================================= #  ===============================  #
-
 //---------------------------------------------------------------------------------------------------
+ 
+       aTests='live in Browser'
+//     aTests='test1 in NodeJS'
 
-      aTests='live in Browser'
-//    aTests='test1 in NodeJS'
+  var aHeadRow = `<tr class="head-row"><td>Name</td><td>Email</td><td>Phone / Mobile</td></tr>`
 
-// var aHeadRow = `<tr class="head-row"><td>Name</td><td>Email</td><td>Phone / Mobile</td></tr>`
+  if ( aTests.match( /test1/ ) ) { 
 
-  if ( aTests.match( /test1/ ) ) {
-
-       var  pJSON    =  parseJSON( '../json/db.json.js' )                               // .(21213.06.1 RJS Was: IODD-members_u2a.json.js) 
+       var  pJSON    =  parseJSON( '../json/IODD-members_u2a.json.js' )
        var  aHTML    =  fmtMembers(  pJSON )
 
             console.log( aHTML )
@@ -48,74 +15,68 @@
 
 //---------------------------------------------------------------------------------------------------
 
-  function  fmtMembers( pJSON ) {
-       var  mMembers =  pJSON.members                                                   // .(21213.06.2 RJS Was pJSON.results.items)  
+function  fmtMembers( pJSON ) {     
+       var  mMembers =  pJSON.members
 
-//     var  aHTML    =  mMembers.map( fmtMember ).join( "\n" )
-       var  aHTML    =  mMembers.sort(sortitem).map( fmtMember ).join( "\n" )
-//     var  mHTMLs=[];  mMembers.forEach( ( pMember, i ) => { fmtMember( pMember, i ) } ); aHTML = mHTMLs.join( "\n" )
+//       var  aHTML    =  mMembers.map( fmtMember ).join( "\n" )
+         var  aHTML    =  mMembers.sort(sortitem).map( fmtMember ).join( "\n" )
+//     var  mHTMLs=[];  mMembers.forEach( ( pMember, i ) => { fmtMember( pMember, i ) } ); aHTML = mHTMLs.join( "\n" ) 
     return  aHTML
-
+            
 //     ---  -------  =  -----------------------------------
 
   function  fmtMember( pMember, i ) {
 
-       var  aClass   =            i % 2 == 1 ? "row-even" : "row-odd"
-//     var  aClass   = "row-" + ( i % 2      ?     "even" :     "odd" )
+       var  aClass = i % 2 == 1 ? "row-even" : "row-odd"
+//     var  aClass = "class=row-" + ( i % 2 ? "even" : "odd" )
 //     var  aClass   =   (  `class="row-even"` )
 
-       var  aMI      =     pMember.Middlename;  aMI = ( aMI  > "" ) ?   ` ${ aMI.substr(0,1) }. ` : ""      // .(21213.06.3 RJS Beg Change case of column names)
+       var  aMI      =     pMember.Middlename;  aMI = ( aMI  > "" ) ?   ` ${ aMI.substr(0,1) }. ` : ""
        var  aName    = `${ pMember.FirstName }${aMI} ${ pMember.LastName }`
-       var  aPhone   =     pMember.Phone1 + ( pMember.Phone2 > ""   ? `, ${ pMember.Phone2  }` : "" )
-            aPhone   =     aPhone == "null" ? "" : aPhone                                                   // .(21213.06.4 RJS Get rid of "null")
-       var  aEmail   =     pMember.Email                                                                    // .(21213.06.3 RJS End) 
+//     var  aPhone   =     pMember.Phone1 + ( pMember.Phone2 > ""   ? `, ${ pMember.Phone2  }` : "" )
+//          aPhone   =     aPhone != "null" ? aPhone : ""
+       var  aPhone1  =     pMember.Phone1
+            aPhone1  =     aPhone1 != null ? aPhone1 : ""
+       var  aPhone2  =     pMember.Phone2
+            aPhone2  =     aPhone2 != null ? aPhone2 : ""
+       var  aEmail   =     pMember.Email
 
        var  aRow     = `  <tr Class="${ aClass }" id="R${ `${ i + 1 }`.padStart( 3, "0" ) }">\n`
-                     + `  <td class="name"><strong><a href="syschangepassword.js?username=${ aName }">${ aName }</a></strong></td>\n`
-                     + `  <td class="email"><small ><a href="mailto:${ aEmail }">Email Address</a></small></td>\n`
-                     + `  <td class="phone"><small >${ aPhone }&nbsp;&nbsp;&nbsp;</small></td>\n`
-                     + `</tr>\n`
+               + `  <td class="name"><a href="syschangepassword.js?username=${ aName }">${ aName }</a></td>\n`
+               + `  <td class="email"><a href="mailto:${ aEmail }"><img class="email-image" src="../assets/images/Email.gif"></a></td>\n`
+               + `  <td class="phone1">${ aPhone1 }</td>\n`
+               + `  <td class="phone2">${ aPhone2}</td>\n`
+               + `</tr>\n`
 
-//          mHTMLs.push( aRow )
-//          aData    = aHeadRow + aRow
-            aData    = aRow
-    return  aData
+//          mHTMLs.push( aRow )                  
+     //aData = aHeadRow + aRow
+     aData = aRow
+     return aData
             }   // eof  fmtMember
 //     ---  -------  =  ----------------------------------
             }   // eof  fmtMembers
 //--------  -------  =  -------------------------------------------------------
 
-   function readFile( aFile ) {                                                         // .(21210.03.1 RAM Beg Add readFile)
+  function  parseJSON(  aFile ) {
        var  pFS      =  require( 'fs' )
        var  aDir     =  __dirname
        var  aText    =  pFS.readFileSync( `${aDir}/${aFile}`, "ASCII" )
-    return  aText
-            }                                                                           // .(21210.03.1 RAM End)
-//--------  -------  =  -------------------------------------------------------
-
-  function  parseJSON(  aFile ) {
-       var  aText    =  readFile( aFile )                                               // .(21210.03.2).
             aText    =  aText.replace( //g, "'" )
             aText    =  aText.replace( //g, "'" )
 
-        if (aFile.match( /\.json$/)) {
+        if (aFile.match( /\.json$/)) {   
        var  pJSON    =  JSON.parse( aText )
             }
-        if (aFile.match( /\.js$/)) {
-            eval( aText )
+        if (aFile.match( /\.js$/)) {   
+            eval( aText )    
             }
 
     return  pJSON
             }   // eof  parseJSON
 //--------  -------  =  -------------------------------------------------------
 
-  function  sortitem( a, b ) {                                                          // .(21209.04.1 RJS Beg Add SortItems)
-    return (a.LastName + a.FirstName) > (b.LastName + b.FirstName) ? 1 : -1             // .(21213.06.5)
-            }                                                                           // .(21209.04.1 RJS End)
+function sortitem(a,b) {
+	return (a.LastName + a.FirstName) > (b.LastName + b.FirstName) ? 1 : -1
+	}
 //---------------------------------------------------------------------------------------------------
 
-//========================================================================================================= #  ===============================  #
-/*\
-##SRCE     +====================+===============================================+
-##RFILE    +====================+=======+===================+======+=============+
-\*/
